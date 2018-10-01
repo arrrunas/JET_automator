67390a9e3eb2445b489f648fa5e40993351a8a7e
Arunas <arunas.umb@gmail.com>
2018-10-01 19:45:47 +0300

    Implemented config importing and menu

--- JET_automator.py
+++ JET_automator.py
@@ -3,12 +3,17 @@
 # arunas.umb@gmail.com
 
 import pandas as pd
-import numpy as np
 import re
 from sys import argv
 
 # filename is specified as script argument, usage: python jet_automator.py filename.csv
-script, filename = argv
+if len(argv) == 3:
+    script, filename, config_file = argv
+elif len(argv) == 2:
+    script, filename = argv
+else:
+    print "Please specify at least the data file as argument, eg. python JET_automator.py data.csv"
+
 
 # import tasks are run
 print '\n'
@@ -24,23 +29,42 @@ print '\nSample data:'
 print data.head()
 print '\n'
 
-# data cleaning to rename columns and remove currency symbols
+# if config is specified, import config settings; otherwise ask for column names to be clarified
 def data_cleaning(data):
-    print "Please specify name of the column containing account number."
-    acc_no = raw_input('> ')
-    print "Please specify name of the column containing debit amounts."
-    debit = raw_input('> ')
-    print "Please specify name of the column containing credit amounts."
-    credit = raw_input('> ')
-    print "Please specify name of the column containing transaction ID."
-    trans_ID = raw_input('> ')
-    print "Please specify name of the column containing user ID."
-    user_ID = raw_input('> ')
-    print "Please specify name of the column containing posting date."
-    post_date = raw_input('> ')
-    print "Please specify name of the column containing entry date."
-    entry_date = raw_input('> ')
+    if len(argv) == 3:
+        with open(config_file, 'rU') as in_file:
+            config_list = in_file.read().split('\n')
+        print '\n%s loaded, parsing column names:' % config_file
+        print config_list
+        # assigning variables from config
+        acc_no = config_list[0]
+        debit = config_list[1]
+        credit = config_list[2]
+        trans_ID = config_list[3]
+        user_ID = config_list[4]
+        post_date = config_list[5]
+        entry_date = config_list[6]
+    elif len(argv) == 2:
+        print "Please specify name of the column containing account number."
+        acc_no = raw_input('> ')
+        print "Please specify name of the column containing debit amounts."
+        debit = raw_input('> ')
+        print "Please specify name of the column containing credit amounts."
+        credit = raw_input('> ')
+        print "Please specify name of the column containing transaction ID."
+        trans_ID = raw_input('> ')
+        print "Please specify name of the column containing user ID."
+        user_ID = raw_input('> ')
+        print "Please specify name of the column containing posting date."
+        post_date = raw_input('> ')
+        print "Please specify name of the column containing entry date."
+        entry_date = raw_input('> ')
+        # writing column information to file
+        config_list = [acc_no, debit, credit, trans_ID, user_ID, post_date, entry_date]
+        with open('config.txt', 'w') as out_file:
+            out_file.write('\n'.join(config_list))
 
+# data cleaning to rename columns and remove currency symbols
     print "\nCalculating."
     # test if amounts are in single column or not
     single_column_amount = False
@@ -116,7 +140,6 @@ def cr_dr_test(data_cleaned, single_column_amount):
 def correspondence(data_cleaned, single_column_amount):
     more_correspondence = True
     while more_correspondence == True:
-        print '\n'
         print '*'*80
         print '\nEnter type of correspondence to do (eg. revenue, cash, etc.) or \'q\' to quit.'
         type_of_correspondence = raw_input("> ")
@@ -147,8 +170,28 @@ def correspondence(data_cleaned, single_column_amount):
         print '*'*80
         print "\n%s correspondence test completed in %s correspondence.txt." % (type_of_correspondence, type_of_correspondence)
 
+# data cleaning launch
 data_cleaned, single_column_amount = data_cleaning(data)
 
-completeness_test(data_cleaned, single_column_amount)
-cr_dr_test(data_cleaned, single_column_amount)
-correspondence(data_cleaned, single_column_amount)
+# application menu 
+stop_app = False
+while stop_app == False:
+    print "\n"
+    print "*"*80
+    print """\nChoose test to perform by typing corresponding number and pressing enter (eg. 1):
+    [1] Completeness
+    [2] CR=DR
+    [3] Correspondence
+    [q] Quit application"""
+    user_input = raw_input ('> ')
+    if user_input == '1':
+        completeness_test(data_cleaned, single_column_amount)
+    elif user_input == '2':
+        cr_dr_test(data_cleaned, single_column_amount)
+    elif user_input == '3':
+        correspondence(data_cleaned, single_column_amount)
+    elif user_input == 'q':
+        stop_app = False
+        break
+    else:
+        print "Input invalid."
